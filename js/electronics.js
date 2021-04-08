5244d59ab3505f3b49f6b35e5e2db2d85ae6c6fb
Familia Porcel <brandon7.7porcel@gmail.com>
2021-04-08 10:50:06 -0300

    ya hice el fetch,voy a modalizar

--- js/electronics.js
+++ js/electronics.js
@@ -1,7 +1,25 @@
 import darkMode from './modules/darkMode.js';
-
 const d = document;
-
+const $fragment = d.createDocumentFragment();
+const $productTemplate = d.getElementById('product-template').content;
+const drawProducts = (info) => {
+	console.log(info);
+	info.forEach((el) => {
+		$productTemplate.querySelector('.product-img').src = el.image;
+		$productTemplate.querySelector('.product-name').textContent = el.title;
+		const $clone = $productTemplate.cloneNode(true);
+		$fragment.appendChild($clone);
+	});
+	d.querySelector('.products').appendChild($fragment);
+};
+const getData = async () => {
+	const res = await fetch(
+		'https://fakestoreapi.com/products/category/electronics'
+	);
+	const data = await res.json();
+	drawProducts(data);
+};
 d.addEventListener('DOMContentLoaded', () => {
 	darkMode();
+	getData();
 });
