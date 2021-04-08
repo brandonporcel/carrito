a09a8a8e15945e98144b688108315ff93c2790d1
Familia Porcel <brandon7.7porcel@gmail.com>
2021-04-08 14:53:29 -0300

    estoy haceindo el pop up, agregue un loader

--- js/electronics.js
+++ js/electronics.js
@@ -1,25 +1,41 @@
 import darkMode from './modules/darkMode.js';
 import getData from './modules/getData.js';
 const d = document;
-const $fragment = d.createDocumentFragment();
-const $productTemplate = d.getElementById('product-template').content;
 const drawProducts = async () => {
+	const $fragment = d.createDocumentFragment();
+	const $productTemplate = d.getElementById('product-template').content;
 	const data = await getData(
-		'https://fakestoreapi.com/products/category/electronics'
+		'https://fakestoreapi.com/products/category/electronics',
+		'../assets/images/loader.svg',
+		'.products'
 	);
-
+	// console.log(data);
 	data.forEach((el) => {
 		$productTemplate.querySelector('.product-img').src = el.image;
+		$productTemplate.querySelector('.product-img').alt = el.title;
 		$productTemplate.querySelector('.product-name').textContent = el.title;
+		$productTemplate.querySelector(
+			'.product-price'
+		).textContent = `$${el.price}`;
 		const $clone = $productTemplate.cloneNode(true);
 		$fragment.appendChild($clone);
 	});
 	d.querySelector('.products').appendChild($fragment);
 };
-// const getData = async () => {
-// 	drawProducts(data);
-// };
+const productPopUp = () => {
+	d.addEventListener('click', (e) => {
+		if (e.target.matches('.product') || e.target.matches('.product *')) {
+			d.querySelector('.pop-up-wrapper').classList.remove('none');
+		}
+		if (e.target.matches('.pop-up-close-btn')) {
+			d.querySelector('.pop-up-wrapper').classList.add('none');
+		} else if (!e.target.matches('.pop-up-ctn *')) {
+			console.log('toco otra cosa');
+		}
+	});
+};
 d.addEventListener('DOMContentLoaded', async () => {
 	darkMode();
 	await drawProducts();
+	productPopUp();
 });
--- js/modules/getData.js
+++ js/modules/getData.js
@@ -1,6 +1,10 @@
-const getData = async (url) => {
+const getData = async (url, loaderUrl, loaderContainer) => {
+	document.querySelector(
+		loaderContainer
+	).innerHTML = `<img  class="loader" src=${loaderUrl} alt="loader">`;
 	const res = await fetch(url);
 	const data = await res.json();
+	document.querySelector(loaderContainer).innerHTML = '';
 	return data;
 };
 export default getData;
