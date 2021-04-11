76d4acb987b8a269cb05b593ad046a5897e42409
Familia Porcel <brandon7.7porcel@gmail.com>
2021-04-11 11:50:48 -0300

    me esta cvotando agregar productos

--- js/electronics.js
+++ js/electronics.js
@@ -1,36 +1,55 @@
 import categoryChange from './modules/categoryChange.js';
 import darkMode from './modules/darkMode.js';
 import drawProducts from './modules/drawProducts.js';
-
 import popUp from './modules/popUp.js';
 const d = document;
-let carrito = [];
+let carrito = {};
 let producto = {};
 let cartLength = carrito.length;
-const $cartBtn = d.querySelector('.carritoBtn');
 
+const $cartBtn = d.querySelector('.carritoBtn');
+const incrementCart = () => {
+	return 2;
+};
+export default incrementCart;
 const cart = () => {
 	const buyBtn = d.querySelector('.pop-up-buy');
 	const addCartBtn = d.querySelector('.pop-up-cart');
 	$cartBtn.textContent = `🛒/${cartLength}`;
 	addCartBtn.addEventListener('click', async (e) => {
 		cartLength += 1;
+
 		$cartBtn.textContent = `🛒/${cartLength}`;
+
 		const nombre = d.querySelector('.pop-up-name').textContent;
 		const precio = d.querySelector('.pop-up-price').textContent;
 		const id = d.querySelector('.pop-up-name').dataset.id;
-
+		let cantidadd = 0;
 		// returns the object. i dont do this cuz i am a boludo and because i dont wanna call the api again
 		// const productInfo = Fecthjson.find((x) => x.id === '45');
-
+		cantidadd++;
 		producto = {
 			nombre,
 			precio,
 			id,
+			cantidad: cantidadd,
 		};
 
-		carrito.push(producto);
-		console.log(carrito);
+		carrito[producto.id] = { ...producto };
+
+		// existe un producto
+		if (producto.hasOwnProperty('id')) {
+			// cantidadd++;
+			// carrito[id].cantidad += 1;
+			// console.log(carrito[id].cantidad, carrito[id].id);
+			// cantidad += 1;
+			// console.log((producto.cantidad += 1), 'producto.cantidad');
+			// console.log(carrito[id], 'carrito[id]');
+			// const a = carrito[id].cantidad++;
+			// console.log(a, 'carrito[id].cantidad++');
+		}
+
+		console.log(carrito, 'carrito');
 	});
 };
 
--- js/money.js
+++ js/money.js
@@ -1,6 +1,13 @@
 import darkMode from './modules/darkMode.js';
-
+import incrementCart from './electronics.js';
 const d = document;
+const $cartBtn = d.querySelector('.carritoBtn');
+let num = incrementCart();
+const changeHeaderCart = () => {
+	$cartBtn.textContent = `🛒/${num}`;
+};
+
 d.addEventListener('DOMContentLoaded', () => {
 	darkMode();
+	changeHeaderCart();
 });
