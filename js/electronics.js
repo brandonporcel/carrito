94903797af8e3333fad8d03cac52a9189c2da9d1
Familia Porcel <brandon7.7porcel@gmail.com>
2021-04-11 12:21:45 -0300

    no puedoo

--- js/electronics.js
+++ js/electronics.js
@@ -12,6 +12,7 @@ const incrementCart = () => {
 	return 2;
 };
 export default incrementCart;
+let cantidadd = 0;
 const cart = () => {
 	const buyBtn = d.querySelector('.pop-up-buy');
 	const addCartBtn = d.querySelector('.pop-up-cart');
@@ -24,31 +25,36 @@ const cart = () => {
 		const nombre = d.querySelector('.pop-up-name').textContent;
 		const precio = d.querySelector('.pop-up-price').textContent;
 		const id = d.querySelector('.pop-up-name').dataset.id;
-		let cantidadd = 0;
 		// returns the object. i dont do this cuz i am a boludo and because i dont wanna call the api again
 		// const productInfo = Fecthjson.find((x) => x.id === '45');
-		cantidadd++;
+		if (carrito.hasOwnProperty(producto.id)) {
+			// cantidadd = carrito[producto.id].cantidad + 1;
+			// carrito[id].cantidad++;
+			console.log(carrito[id]);
+			carrito[id].cantidad++;
+			producto.cantidad = carrito[producto.id].cantidad + 1;
+		}
 		producto = {
 			nombre,
 			precio,
 			id,
 			cantidad: cantidadd,
 		};
+		// carrito[id].cantidadd = 0;
 
-		carrito[producto.id] = { ...producto };
+		// carrito[producto.id].cantidad++;
+		// console.log(producto.cantidad);
 
 		// existe un producto
-		if (producto.hasOwnProperty('id')) {
-			// cantidadd++;
-			// carrito[id].cantidad += 1;
-			// console.log(carrito[id].cantidad, carrito[id].id);
-			// cantidad += 1;
-			// console.log((producto.cantidad += 1), 'producto.cantidad');
-			// console.log(carrito[id], 'carrito[id]');
-			// const a = carrito[id].cantidad++;
-			// console.log(a, 'carrito[id].cantidad++');
-		}
-
+		// cantidadd++;
+		// carrito[id].cantidad += 1;
+		// console.log(carrito[id].cantidad, carrito[id].id);
+		// cantidad += 1;
+		// console.log((producto.cantidad += 1), 'producto.cantidad');
+		// console.log(carrito[id], 'carrito[id]');
+		// const a = carrito[id].cantidad++;
+		// console.log(a, 'carrito[id].cantidad++');
+		carrito[producto.id] = { ...producto };
 		console.log(carrito, 'carrito');
 	});
 };
