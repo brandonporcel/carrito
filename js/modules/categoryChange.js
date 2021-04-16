4b7f073766aed8256f19871a19654ae44ed8b33c
Brandon Porcel <brandon7.7porcel@gmail.com>
2021-04-16 15:33:49 -0300

    Pense en que cuando haga click en el carrito del home muestre el summary pero paja

--- js/modules/categoryChange.js
+++ js/modules/categoryChange.js
@@ -3,8 +3,7 @@ const categoryChange = () => {
 	const $select = d.getElementById('sort-by-category');
 	$select.addEventListener('change', () => {
 		if ($select.value === 'jewelery') {
-			// hide products.show summary
-			// window.location.pathname = '/pages/jewelery.html';
+			window.location.pathname = '/pages/jewelery.html';
 		}
 	});
 };
