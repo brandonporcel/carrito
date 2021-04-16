41c7eeeddcd95116c8d1206a2a9f445aec025408
Brandon Porcel <brandon7.7porcel@gmail.com>
2021-04-16 17:04:07 -0300

    termine...

--- js/money.js
+++
@@ -1,8 +0,0 @@
-import darkMode from './modules/darkMode.js';
-
-const d = document;
-const addCartBtn = d.querySelector('.pop-up-cart');
-
-d.addEventListener('DOMContentLoaded', () => {
-	darkMode();
-});
